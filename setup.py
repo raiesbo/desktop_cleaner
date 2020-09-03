1330f3585b11445d9d65089030611fe74f369e1d
raiesbo <raiesbo8@gmail.com>
2020-09-03 16:50:37 +0200

    new steps

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 import os
 
 
-
 desktop = f"C:\\Users\\usuario\\Desktop"
 
-class files:
-    def __init__(self, extension):
-        self.extension = extension
+
+class Files:
+    def __init__(self):
+        #self.extension = extension
         self.files = os.listdir(desktop)
     
     def create_folder(self):
         for file in self.files:
             file_list = file.split(".", 1)
-            if  len(file_list) = 2 and file_list[1] not in desktop:
-                os.makedir(desktop + "//" + file_list[1])
-            os.rename(desktop + "//" + "file", desktop + "//" + file_list[1] + "//" + "file")
+            if  len(file_list) == 2 and file_list[1] not in desktop:
+                os.mkdir(desktop + "//" + file_list[1])
+            if  len(file_list) == 2 and len(file_list) != "exe":
+                os.rename(desktop + "//" + "file", desktop + "//" + file_list[1] + "//" + "file")
+
+
+fl1 = Files()
+
+fl1.create_folder()
